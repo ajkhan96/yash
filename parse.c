5fab88e6aa511ec4c4616d69116cf4c97746d55d
ajkhan96 <ajkhan96@gmail.com>
2017-09-19 01:46:29 -0500

    File redirection works for one command. Piping works but offsets prompt somehow. Will need to investigate. Main needs rearranging, as fg, bg, jobs not implemented

--- parse.c
+++ parse.c
@@ -5,29 +5,34 @@
 #define TRUE 1
 #define FALSE 0
 
+char* tokenBuffer;
+
 int isSpecialToken(char* s){
     return !(strcmp(s, "<")*strcmp(s, ">")*strcmp(s, "2>")*strcmp(s, "|")*strcmp(s, "&")*strcmp(s, "\n"));
 }
 
-void checkForSpecialCharacters(char* tokenBuffer, int firstCommand){
+void checkForSpecialCharacters(int firstCommand){
     if(!strcmp(tokenBuffer, "<")){
         tokenBuffer = strtok(NULL, " ");
         if(firstCommand) firstCommandInputToken = strdup(tokenBuffer);
-        else secondChildInputToken = strdup(tokenBuffer);
+        else secondCommandInputToken = strdup(tokenBuffer);
     }else if(!strcmp(tokenBuffer, ">")){
         tokenBuffer = strtok(NULL, " ");
         if(firstCommand) firstCommandOutputToken = strdup(tokenBuffer);
-        else secondChildOutputToken = strdup(tokenBuffer);
+        else secondCommandOutputToken = strdup(tokenBuffer);
     }else if(!strcmp(tokenBuffer, "2>")){
         tokenBuffer = strtok(NULL, " ");
         if(firstCommand) firstCommandErrorToken = strdup(tokenBuffer);
-        else secondChildErrorToken = strdup(tokenBuffer);
+        else secondCommandErrorToken = strdup(tokenBuffer);
     }
 }
 
 void parseInput(char* line){
+    firstCommandInputToken = firstCommandOutputToken = firstCommandErrorToken = NULL;
+    secondCommandInputToken = secondCommandOutputToken = secondCommandErrorToken = NULL;
+    piped = backgrounded = FALSE;
     strtok(line, "\n");
-    char* tokenBuffer = strtok(line, " ");
+    tokenBuffer = strtok(line, " ");
     int i = 0;
     while(tokenBuffer != NULL && !isSpecialToken(tokenBuffer) && i < MAX_INPUT_ARGS){
         firstCommand[i] = strdup(tokenBuffer);
@@ -39,9 +44,29 @@ void parseInput(char* line){
     int j = 0;
     for(j = 0; j < 3; j++){
         if(tokenBuffer != NULL){
-            checkForSpecialCharacters(tokenBuffer, TRUE);
+            checkForSpecialCharacters(TRUE);
         }    
     }
 
-
+    if(tokenBuffer != NULL){
+        if(!strcmp(tokenBuffer, "|")){
+            piped = TRUE;
+            tokenBuffer = strtok(NULL, " ");
+            i = 0;
+            while(tokenBuffer != NULL && !isSpecialToken(tokenBuffer) && i < MAX_INPUT_ARGS){
+                secondCommand[i] = strdup(tokenBuffer);
+                tokenBuffer = strtok(NULL, " ");
+                i++;
+            }
+            secondCommand[i] = NULL;
+            for(j = 0; j < 3; j++){
+                if(tokenBuffer != NULL){
+                    checkForSpecialCharacters(FALSE);
+                }    
+            }
+        }
+    }
+    if(tokenBuffer != NULL && !strcmp(tokenBuffer, "&")){
+        backgrounded = TRUE;
+    }
 }
--- yash.c
+++ yash.c
@@ -3,12 +3,18 @@
 #include <signal.h>
 #include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include "parse.h"
+
+#define MAX_JOBS 100
 
 int status;
 pid_t pid_ch1, pid_ch2, pid;
 char lineBuffer[MAX_LINE_LENGTH];
+int inFile, outFile, errFile;
+int pipefd[2];
 
 static void sig_int(int signo){
     kill(-pid_ch1, SIGINT);
@@ -25,23 +31,61 @@ int main(void){
 
     while (1){
         write(1, "# ", 2);
+        //printf("# ");
         if(!fgets(lineBuffer, MAX_LINE_LENGTH, stdin)) break;
         parseInput(lineBuffer);
+        if(pipe(pipefd) == -1) fprintf(stderr, "Failed to create pipe");
         pid_ch1 = fork();
         if (pid_ch1 == 0)
         {
             //child 1
+            setpgid(getpid(), getpid());
+            if(firstCommandInputToken){
+                if((inFile = open(firstCommandInputToken, O_RDONLY)) < 0) fprintf(stderr, "Could not find file %s\n", firstCommandInputToken);
+                else if(dup2(inFile, STDIN_FILENO) != STDIN_FILENO) fprintf(stderr, "Dup 2 for stdin failed\n");
+                close(inFile);
+            }
+            if(firstCommandOutputToken){
+                if((outFile = open(firstCommandOutputToken, O_WRONLY | O_APPEND | O_CREAT, S_IWUSR)) < 0) fprintf(stderr, "Could not create file %s\n", firstCommandOutputToken);
+                else if(dup2(outFile, STDOUT_FILENO) != STDOUT_FILENO) fprintf(stderr, "Dup 2 for stdout failed\n");
+                close(outFile);
+            }
+            if(firstCommandErrorToken){
+                if((errFile = open(firstCommandErrorToken, O_WRONLY | O_APPEND | O_CREAT, S_IWUSR)) < 0) fprintf(stderr, "Could not create file %s\n", firstCommandErrorToken);
+                else if(dup2(errFile, STDERR_FILENO) != STDERR_FILENO) fprintf(stderr, "Dup 2 for stderr failed\n");
+                close(errFile);
+            }
+            if(piped){
+                close(pipefd[0]);
+                dup2(pipefd[1], STDOUT_FILENO);
+            }
             execvp(firstCommand[0], firstCommand);
         }
         else if (pid_ch1 > 0)
         {
             //parent
+            if(piped){
+                pid_ch2 = fork();
+                if(pid_ch2 == 0){
+                    //child 2
+                    if(secondCommandOutputToken){
+                        if((outFile = open(secondCommandOutputToken, O_WRONLY | O_APPEND | O_CREAT, S_IWUSR)) < 0) fprintf(stderr, "Could not create file %s\n", secondCommandOutputToken);
+                        else if(dup2(outFile, STDOUT_FILENO) != STDOUT_FILENO) fprintf(stderr, "Dup 2 for stdout failed\n");
+                        close(outFile);
+                    }
+                    if(secondCommandErrorToken){
+                        if((errFile = open(secondCommandErrorToken, O_WRONLY | O_APPEND | O_CREAT, S_IWUSR)) < 0) fprintf(stderr, "Could not create file %s\n", secondCommandErrorToken);
+                        else if(dup2(errFile, STDERR_FILENO) != STDERR_FILENO) fprintf(stderr, "Dup 2 for stderr failed\n");
+                        close(errFile);
+                    }
+                    close(pipefd[1]);
+                    dup2(pipefd[0], STDIN_FILENO);
+                    execvp(secondCommand[0], secondCommand);
+                }
+            }
+            close(pipefd[0]);
+            close(pipefd[1]);
             pid = waitpid(-1, &status, WUNTRACED | WCONTINUED);
-        }
-        else
-        {
-            fprintf(stderr, "Fork of Child 1 Failed! Exiting");
-            exit(1);
         }
     }
 }
